18ac0df540b93571b4ee3d2c26e326ce8e37128e
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-12-10 10:33:16 -0500

    dot on screen and disappear when hit by snake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,7 @@ screen.title("my snake game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,6 +24,9 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
