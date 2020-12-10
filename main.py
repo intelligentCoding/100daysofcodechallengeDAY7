769c832aef8e1a39cc5a5cf6dc68dfad63974f88
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-12-10 10:51:20 -0500

    Increase score when eat the dot

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from turtle import Screen
 from snake import Snake
 from food import Food
 import time
+from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -11,7 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -27,6 +28,8 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
+
 
 
 screen.exitonclick()
